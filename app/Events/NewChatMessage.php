b22ce94d7adf171dd4fa2d128552ba31bfc3b7f4
Enes Alili <e.alili@ineting.net>
2023-04-15 23:32:34 +0200

    first commit

---
+++ app/Events/NewChatMessage.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
+
+class NewChatMessage implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct(
+        public $chatMessage,
+        public $user
+    )
+    {
+        //
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, Channel>
+     */
+    public function broadcastOn(): array
+    {
+        Log::info('Event fired');
+        return [
+            new PresenceChannel('chat.room.' . $this->chatMessage->chat_room_id),
+        ];
+    }
+
+    public function broadcastAs(): string
+    {
+        return 'room.messages';
+    }
+
+    /**
+     * Get the data to broadcast.
+     *
+     * @return array<string, mixed>
+     */
+    public function broadcastWith(): array
+    {
+        return [
+            'user' => [
+                'id' => $this->user->id,
+                'name' => $this->user->name,
+            ],
+            'message' => $this->chatMessage
+        ];
+    }
+}
---
+++ app/Http/Controllers/ChatController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Events\NewChatMessage;
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Models\ChatRoom;
+use App\Models\ChatMessage;
+use Illuminate\Support\Facades\Auth;
+
+class ChatController extends Controller
+{
+    public function rooms(Request $request)
+    {
+        return ChatRoom::all();
+    }
+
+    public function messages(Request $request, $roomId)
+    {
+        return ChatMessage::where('chat_room_id', $roomId)
+            ->with('user')
+            ->orderBy('created_at', 'DESC')
+            ->get();
+    }
+
+    public function newMessage(Request $request, $roomId)
+    {
+        $newChatMessage = new ChatMessage();
+        $newChatMessage->user_id = Auth::id();
+//        $user = User::find(Auth::id());
+        $newChatMessage->chat_room_id = $roomId;
+        $newChatMessage->message = $request->message;
+
+        $newChatMessage->save();
+
+        event(new NewChatMessage($newChatMessage, Auth::user()));
+
+//        broadcast(new NewChatMessage($newChatMessage))->toOthers();
+
+        return $newChatMessage;
+    }
+
+}
